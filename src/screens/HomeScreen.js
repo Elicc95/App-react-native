91aa4fd1d2380cbf11fa12b0d6667e11ba19fd97
Oscar <oscarpulga96@gmail.com>
2020-05-18 19:34:35 -0500

    add[18-05-2020] styles added to form

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TextInput } from "react-native";
-
+import t from 'tcomb-form-native'
 
 const HomeScreen = (props) => {
-  return <View>
-  <Text>Usuario</Text>
-  <TextInput
-    style= {
-        styles.textInput
-      }
-  />
+
+
+  return <View style={styles.content}>
+  <Text style={{justifyContent: "center"}}>Usuario</Text>
+  <TextInput style= { styles.textInput}/>
   <Text>Contraseña</Text>
   <TextInput
   secureTextEntry= {true}
@@ -26,6 +24,12 @@ const HomeScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
+  content:{
+    justifyContent:   'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff'
+  },
   text: {
     fontSize: 30
   },
